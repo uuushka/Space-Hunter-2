dc8145c771522afcc67871eee7d519c511b14add
uuushka <Yushka1988#>
2020-10-12 03:56:33 +0300

    start work

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [SerializeField] private int score = 0;
+    [SerializeField] private int health = 3;
+    [SerializeField] private int level = 1;
+    
+    private void Awake()
+    {
+        int gameStatusesCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusesCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int Score
+    {
+        get => score;
+        set => score = value;
+    }
+
+    public int Health
+    {
+        get => health;
+        set => health = value;
+    }
+
+    public int Level
+    {
+        get => level;
+        set => level = value;
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int currentSceneIndex = 0;
+    [SerializeField] private float timeToPause = 1f;
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(WaitFor());
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator WaitFor()
+    {
+        yield return new WaitForSeconds(timeToPause);
+    }
+
+    public void LoadStartScreen()
+    {
+        StartCoroutine(WaitFor());
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameOverScreen()
+    {
+        // todo 
+    }
+}
